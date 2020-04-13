811f99df58981bc64caddcac3adac1921011b200
Rizky Luthfianto <mrluthfianto@gmail.com>
2020-04-13 17:53:16 +0700

    tes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='ncvx',
-    version='0.1.8',
+    version='0.1.9',
     author='Steven Diamond, Reza Takapoui, Stephen Boyd',
      author_email='diamond@cs.stanford.edu, takapoui@stanford.edu, boyd@stanford.edu',
     packages=['ncvx'],
     license='GPLv3',
     zip_safe=False,
-    install_requires=["cvxpy >= 0.3.5", "lap", "scsprox"],
+    install_requires=["cvxpy >= 1.0.0", "lap", "scsprox"],
     use_2to3=True,
     url='http://github.com/cvxgrp/ncvx/',
     description='A CVXPY extension for problems with convex objective and decision variables from a nonconvex set.',
